df61fc20285faf064a2ffacf3b24650af75e25cc
Maicon Machado <147193871+maiconarroyo@users.noreply.github.com>
2024-09-22 12:23:06 -0300

    progress bar

--- js/components/smooth-scroll.js
+++ js/components/smooth-scroll.js
@@ -1,18 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector(".main-header");
-    let lastScrollTop = 0; // Armazena a última posição de rolagem
-
+    let lastScrollTop = 0; 
     window.addEventListener("scroll", function () {
         let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
         if (currentScrollTop > lastScrollTop) {
             // Rolando para baixo
-            header.style.top = "-70px"; // Ajuste o valor conforme a altura do seu cabeçalho
+            header.style.top = "-70px"; 
         } else {
-            // Rolando para cima
             header.style.top = "0";
         }
         
-        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // Para navegadores compatíveis com back-to-top
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; 
     });
 })
